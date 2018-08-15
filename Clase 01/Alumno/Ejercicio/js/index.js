400923608ea0f40187d9a328555e0298f846b511
Facundo <fbritez@igdigital.com>
2018-08-14 21:41:47 -0300

    1er Commit

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+//var nombre = prompt('Ingresá tu nombre')
+//var mensaje = 'Hola ' + nombre 
+//console.log(mensaje)
+
+
+//var dolar = prompt('Cuantos dólares tenés?')
+//var parsedDolar = parseFloat(dolar, 10)
+//console.log(parsedDolar + 30)
+
+//var dolar = prompt('Cuantos dólares tenés?')
+//var parsedDolar = parseFloat(dolar, 10)
+//console.log(parsedDolar - 10
+
+//var unNumero = prompt('Dame un número')
+//var otroNumero = prompt('Dame otro número')
+
+
+//var parsedunNumero = parseFloat(unNumero, 10)
+//var parsedotroNumero = parseFloat(otroNumero, 10)
+
+
+
+//console.log(parsedunNumero - parsedotroNumero)
