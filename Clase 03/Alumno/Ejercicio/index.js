11e905fbcc41772657fdcd4acfeb613f4fd7d97a
Facundo <facundo001@MacBook-Pro-de-Facundo.local>
2018-08-24 21:26:50 -0300

    ejercicio clase 3

---
+++ Clase 03/Alumno/Ejercicio/index.js
@@ -0,0 +1,25 @@
+//
+//
+
+var dayOfTheWeeks = [
+  'Lunes',
+  'Martes',
+  'Miércoles',
+  'Miercoles',
+  'Jueves',
+  'Viernes',
+  'Sábado',
+  'Sabado',
+  'Domingo',
+  'Pato'
+]
+
+var dayOfTheWeek
+
+console.log('Dias de la semana: ')
+
+for (var i = 0; i < dayOfTheWeeks.length; i++) {
+  dayOfTheWeek = dayOfTheWeeks[i]
+
+  console.log(dayOfTheWeek + ' dia de la semana')
+}
